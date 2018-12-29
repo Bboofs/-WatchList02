9f7732cc2c7fe361787e2a60efb340d3a3023e99
bboofs <bboofs@yahoo.com>
2018-12-29 14:05:39 +0300

    Add Wednesday: Request Object - Search for Movies
    TOPIC: Wednesday: Request Object - Search for Movies. PAGE: https://moringaschool.instructure.com/courses/46/pages/wednesday-request-object?module_item_id=2218. SECTION: Search for Movies. Implemented search functionality by adding search form in index.html, submitting the request and redirecting results on 29-12-2018

--- app/error.py
+++ app/error.py
@@ -1,11 +1,12 @@
 from flask import render_template
 from app import app
 
+
 @app.errorhandler(404)
 def four_Ow_four(error):
     '''
-    function o render the 404 error page
+    function to render the 404 error page
     :param error:
     :return:
     '''
-    return render_template('fourOwfour.html'),404
+    return render_template('fourOwfour.html'), 404
